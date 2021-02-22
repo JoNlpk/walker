514b0d7ba9c64eb461ec90fa45ceae86efa8facb
JoNlpk <77628161+JoNlpk@users.noreply.github.com>
2021-02-22 12:37:45 -0500

    Add files via upload

---
+++ email.sh
@@ -0,0 +1,134 @@
+#!bin/bash
+
+clear
+
+#agregemos un banner
+sleep 3
+
+						setterm -foreground magenta
+echo -e "
+[0_0]                  				           …………………▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄
+							  ……………▄▄█▓▓▓▒▒▒▒▒▒▒▒▒▒▓▓▓▓█▄▄
+							  …………▄▀▀▓▒░░░░░░░░░░░░░░░░▒▓▓▀▄
+							  ………▄▀▓▒▒░░░░░░░░░░░░░░░░░░░▒▒▓▀▄
+							 ……..█▓█▒░░░░░░░░░░░░░░░░░░░░░▒▓▒▓█
+							 …..▌▓▀▒░░░░░░░░░░░░░░░░░░░░░░░░▒▀▓█
+						        …..█▌▓▒▒░░░░░░░░░░░░░░░░░░░░░░░░░▒▓█
+						         …▐█▓▒░░░░░░░░░░░░░░░░░░░░░░░░░░░▒▓█▌
+							 …█▓▒▒░░░░░░░░░░░░░░░░░░░░░░░░░░░░▒▓█
+					               ..█▐▒▒░░░░░░░░░░░░░░░░░░░░░░░░░░░▒▒█▓█
+						        …█▓█▒░░░░░░░░░░░░░░░░░░░░░░░░░░░▒█▌▓█
+						       ..█▓▓█▒░░░░▒█▄▒▒░░░░░░░░░▒▒▄█▒░░░░▒█▓▓█
+						       ..█▓█▒░▒▒▒▒░░▀▀█▄▄░░░░░▄▄█▀▀░░▒▒▒▒░▒█▓█
+						  	.█▓▌▒▒▓▓▓▓▄▄▄▒▒▒▀█░░░░█▀▒▒▒▄▄▄▓▓▓▓▒▒▐▓█
+							.██▌▒▓███▓█████▓▒▐▌░░▐▌▒▓████▓████▓▒▐██
+						       ..██▒▒▓███▓▓▓████▓▄░░░▄▓████▓▓▓███▓▒▒██
+						        .█▓▒▒▓██████████▓▒░░░▒▓██████████▓▒▒▓█
+							..█▓▒░▒▓███████▓▓▄▀░░▀▄▓▓███████▓▒░▒▓█
+		    ─────▄───▄       		         ….█▓▒░▒▒▓▓▓▓▄▄▄▀▒░░░░░▒▀▄▄▄▓▓▓▓▒▒░▓█			─────▄───▄
+	          ─▄█▄─█▀█▀█─▄█		         	……█▓▒░▒▒▒▒░░░░░░▒▒▒▒░░░░░░▒▒▒▒░▒▓█			─▄█▄─█▀█▀█─▄█▄
+		  ▀▀████▄█▄████▀▀			………█▓▓▒▒▒░░██░░▒▓██▓▒░░██░░▒▒▒▓▓█			▀▀████▄█▄████▀▀
+		  ─────▀█▀█▀       		         ………▀██▓▓▓▒░░▀░▒▓████▓▒░▀░░▒▓▓▓██▀			─────▀█▀█▀
+							………….░▀█▓▒▒░░░▓█▓▒▒▓█▓▒░░▒▒▓█▀░				
+							…………█░░██▓▓▒░░▒▒▒░▒▒▒░░▒▓▓██░░█				
+							………….█▄░░▀█▓▒░░░░░░░░░░▒▓█▀░░▄█				
+							…………..█▓█░░█▓▒▒▒░░░░░▒▒▒▓█░░█▓█				
+							…………….█▓█░░█▀█▓▓▓▓▓▓█▀░░█░█▓█▌					
+							……………..█▓▓█░█░█░█▀▀▀█░█░▄▀░█▓█					
+							……………..█▓▓█░░▀█▀█░█░█▄█▀░░█▓▓█					
+						  	 ………………█▓▒▓█░░░░▀▀▀▀░░░░░█▓▓█					
+							 ………………█▓▒▒▓█░░░░ ░░░░░░░█▓▓█
+							………………..█▓▒▓██▄█░░░▄░░▄██▓▒▓█
+							………………..█▓▒▒▓█▒█▀█▄█▀█▒█▓▒▓█
+							………………..█▓▓▒▒▓██▒▒██▒██▓▒▒▓█
+							 ………………….█▓▓▒▒▓▀▀███▀▀▒▒▓▓█
+							  ……………………▀█▓▓▓▓▒▒▒▒▓▓▓▓█▀
+|_[Lpk]_|						 ………………………..▀▀██▓▓▓▓██▀
+"
+				sleep 3
+							setterm -foreground cyan
+echo -e "
+
+					██╗    ██╗███████╗██╗      ██████╗ ██████╗ ███╗   ███╗███████╗
+					██║    ██║██╔════╝██║     ██╔════╝██╔═══██╗████╗ ████║██╔════╝
+					██║ █╗ ██║█████╗  ██║     ██║     ██║   ██║██╔████╔██║█████╗  
+					██║███╗██║██╔══╝  ██║     ██║     ██║   ██║██║╚██╔╝██║██╔══╝  
+					╚███╔███╔╝███████╗███████╗╚██████╗╚██████╔╝██║ ╚═╝ ██║███████╗
+ 				        ╚══╝╚══╝ ╚══════╝╚══════╝ ╚═════╝ ╚═════╝ ╚═╝     ╚═╝╚══════╝
+
+"
+sleep 1
+			setterm -foreground green
+read -p " ║█║▌║█║▌│║▌█║▌║-┣▇▇▇═─DALE ENTER Para Continuar [:!] :  	" enter
+sleep 2
+			setterm -foreground red
+echo -e "
+
+	▄███▄   █▀▄▀█ ██   ▄█ █     				──▀▀▀▀▀▀▀▀▀▀▀▄▄▀▀▀▀▀▀▀▀▀▀▀
+	█▀   ▀  █ █ █ █ █  ██ █     				────────────█▀▀█
+	██▄▄    █ ▄ █ █▄▄█ ██ █     				───────────█▓▓▓▓█
+	█▄   ▄▀ █   █ █  █ ▐█ ███▄  				───────══▄▀█▓▓▓▓█▀▄══
+	▀███▀      █     █  ▐     ▀ 				──▄▄▄▄▄▄▄█▒█▓▓▓▓█▒█▄▄▄▄▄▄▄
+             ▀     █           					──█▀▀▀▀█▀███▄▓▓▄███▀█▀▀▀▀█
+                  ▀						─▄█▄──▄█▄───▀██▀───▄█▄──▄█
+            							─█▒█──█▒█──────────█▒█──█▒█
+								─▀▀▀──▀▀▀──────────▀▀▀──▀▀▀
+"
+sleep 1
+# ahora vamoas agregar un read que corresponde al sujeto que esta enviando el email a este input le asignare la variable name
+			setterm -foreground blue
+read -p       " [!] Coloca El Nombre De Quien Envia El Email [ ] : "  name
+echo 
+# Ahora agregare otro read que corresponde para ingresar el email de quien lo envie a este read le asignare la variable correo
+echo -e "\033[43;320m ------------------------------------------------------------------------------------------------------------------------"
+echo
+read -p "[?] Coloca Un Correo true o False [ ] :  " correo
+echo
+echo -e "\033[43;320m ------------------------------------------------------------------------------------------------------------------------"
+echo
+#Ahora asignare otro read para ingresar el correo de la victima
+read -p "[_] Coloca El Correo De la victima [ ] : " victima
+echo
+#Ahora asignaremos otro read para ingresarm el tema del email'
+echo
+echo -e "\033[43;320m ------------------------------------------------------------------------------------------------------------------------"
+echo
+read -p "[ ] ....Tema.... [ ] : " tema
+echo
+echo -e "\033[43;320m ------------------------------------------------------------------------------------------------------------------------"
+echo
+# y Finalizamos con el read para ingresar el mensaje 
+read -p "...[Mensaje .. ].. : " mensaje
+echo
+echo -e "\033[43;320m ------------------------------------------------------------------------------------------------------------------------"
+echo
+# Ahora vamos a usar curl... este sirve para la transferencia de archivos u descargas etc...
+# bien usaremos --data para indicar q mandaremos valores
+
+curl --data "fromname=$name && fromemail=$correo && replyname=$name && replyemail=$email && to=$victima && subject=$tema && message=$mensaje" http://pruebaemailspoof.000webhostapp.com/ephp_spoofer/ephp_spoofer.php -o spoof >/dev/null 2>&1
+
+# Ahora vamos a verificar si el email se envio o no.. para esto lo vamos a hacer por medio de una condicion
+# Aqui decimos si el archivo spoof existe ... este archivo se crea cuando anadimos la opcion -o en curl
+if [[ -f spoof ]];then
+# Aqui agregamos una variable para confirmaciom
+confirm=$(grep -o "Successfully" spoof)
+# Aqui solo estamos  diciendo q nos recoja UNA palabra que se llame successfully si esta palabra es5ta en el archivo significa que se ha enviado correctamente
+# ahora de donde sale esta palabra ? de la pagina web spoof.000.....
+if [[ $confirm == "Successfully" ]];then
+		setterm -foreground magenta
+  echo "...El Correo Se Envio Correctamente..GRACIAS POR SU TIEMPO.. "
+	sleep 2
+  echo "...lpk..Traveler..0549.. ..No me hago Responsable de su Uso"
+# Tenemos que eliminar el archivo para que no halga errores mas adelante
+rm spoof
+
+else 
+    echo "...NO Se Pudo Enviar El Email.."
+	rm spoof
+fi
+
+else
+	echo ".-.Comprueba Tu conexion..."
+fi
+
+
